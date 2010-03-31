f21b699a66378298d3a0978c15d62db90db431ee
Stephane Chomat <Stephane.Chomat@imag.fr>
2010-03-31 14:41:47 +0200

    add input

--- src/main/java/fr/imag/adele/cadse/script/groovy/DGroovyEditor.java
+++ src/main/java/fr/imag/adele/cadse/script/groovy/DGroovyEditor.java
@@ -1,10 +1,13 @@
 package fr.imag.adele.cadse.script.groovy;
 
 import org.codehaus.groovy.eclipse.editor.GroovyEditor;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.text.ITextListener;
 import org.eclipse.jface.text.TextEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IPersistableElement;
 
 import fr.imag.adele.cadse.core.ui.RuningInteractionController;
 import fr.imag.adele.cadse.si.workspace.uiplatform.swt.ui.DAbstractField;
@@ -27,6 +30,39 @@ public class DGroovyEditor extends DAbstractField<RuningInteractionController> i
 	public void createControl(Composite container, int hspan) {
 		groovyEditor = new GroovyEditor();
 		
+		IEditorInput input = new IEditorInput() {
+			
+			@Override
+			public Object getAdapter(Class adapter) {
+				return null;
+			}
+			
+			@Override
+			public String getToolTipText() {
+				return "text";
+			}
+			
+			@Override
+			public IPersistableElement getPersistable() {
+				return null;
+			}
+			
+			@Override
+			public String getName() {
+				return "script text";
+			}
+			
+			@Override
+			public ImageDescriptor getImageDescriptor() {
+				return null;
+			}
+			
+			@Override
+			public boolean exists() {
+				return false;
+			}
+		};
+		groovyEditor.setInput(input);
 		groovyEditor.createPartControl(container);
 		_mainControl = groovyEditor.getViewer().getTextWidget();
 		groovyEditor.getViewer().addTextListener(this);
